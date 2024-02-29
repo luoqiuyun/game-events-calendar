319d8ec31b7798130bf84ba7952a0a7135530912
luoqiuyun <qiuyun.luo@outlook.com>
2024-02-28 21:07:49 -0800

    unit tests

--- src/components/Month/index.test.tsx
+++ src/components/Month/index.test.tsx
@@ -15,17 +15,23 @@ describe('Month Component', () => {
       </Router>
     );
 
-    const element = getByTestId('card-1');
-    expect(element).toBeInTheDocument();
+    const element1 = getByTestId('card-1');
+    expect(element1).toBeInTheDocument();
+    const element25 = getByTestId('card-25');
+    expect(element25).toBeInTheDocument();
   });
 
   it('shows SelectedGame component when a game is selected', () => {
-    const { getByTestId, getByText } = render(
+    const { getByTestId, getByText, container } = render(
       <Router>
         <Month calendar={mockCalendar} games={mockGames} />
       </Router>
     );
-    fireEvent.click(getByTestId('card-2'));
+
+    const elem = container.getElementsByClassName("day-with-game")[0];
+    fireEvent.click(elem);
+    expect(screen.getByText('Learn More')).toBeInTheDocument();
+    expect(screen.getByText('Pre Order Now')).toBeInTheDocument();
   });
 
 });
