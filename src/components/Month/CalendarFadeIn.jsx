9538d639d84c85783d9c7e3033caacc10480e9c8
luoqiuyun <qiuyun.luo@outlook.com>
2024-03-01 11:53:23 -0800

    transition delay 0.1

--- src/components/Month/CalendarFadeIn.jsx
+++ src/components/Month/CalendarFadeIn.jsx
@@ -7,7 +7,7 @@ function CalendarFadeIn({ children }) {
       initial="hidden"
       whileInView="visible"
       viewport={{ once: true }}
-      transition={{ duration: 0.8 }}
+      transition={{ duration: 0.9, delay: 0.1 }}
       variants={{
         visible: { opacity: 1, scale: 1 },
         hidden: { opacity: 0.2, scale: 0.2 }
